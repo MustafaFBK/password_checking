f735f0eef982e93146c738abbf3c08c8370cb53b
Mustafa Banikhalaf <103966776+MustafaFBK@users.noreply.github.com>
2024-05-13 00:11:22 +0300

    Update Passchk.java

--- Passchk.java
+++ Passchk.java
@@ -1,23 +1,34 @@
 import java.util.Scanner;
-class password_checking{
-    public static void main(String[] args){
-        System.out.println("My name is mustafa");
-        Scanner scan = new Scanner(System.in);
-        System.out.println("----------------------------");
-        int cout = 1 ;
-        do{
-            System.out.print("Enter Your password : ");
-            String password= scan.nextLine();
-            if (password.equals("Mustafa")){
-            System.out.println("You are Crack Me !!!");
-            break;}
-            else{
-            System.out.println("Your input invalid " );
-            if(cout==3){
-            System.out.print("The number of attempts has expired");
-            break;
-            }
-            }
-        }while(cout++<=3);
+
+public class PasswordChecker {
+    private static final int MAX_ATTEMPTS = 3;
+
+    public static void main(String[] args) {
+        System.out.println("My name is Mustafa");
+        try (Scanner scanner = new Scanner(System.in)) {
+            int attemptCount = 1;
+            do {
+                System.out.print("Enter your password: ");
+                String password = scanner.nextLine();
+                if (isValidPassword(password)) {
+                    System.out.println("You are Crack Me !!!");
+                    break;
+                } else {
+                    System.out.println("Your input is invalid.");
+                    if (attemptCount == MAX_ATTEMPTS) {
+                        System.out.println("The number of attempts has expired.");
+                        break;
+                    }
+                }
+                attemptCount++;
+            } while (attemptCount <= MAX_ATTEMPTS);
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+
+    private static boolean isValidPassword(String password) {
+        // TO DO: implement a more secure password validation mechanism
+        return password.equals("Mustafa");
     }
 }
